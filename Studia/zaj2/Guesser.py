3dd06c5931e5f4bef0b9f8620e266abcbfb5ed6a
Patryk Weber <weberpatryk7@gmail.com>
2018-10-20 19:32:01 +0200

    Updating Guesser file to have base for OOP version

--- Studia/zaj2/Guesser.py
+++ Studia/zaj2/Guesser.py
@@ -1,29 +1,51 @@
 import random
 
-slowa = ("python","gdansk","dlaczego","gdynia","wsb")
-word = random.choice(slowa)
-
-poprawnie = word
-pomie = ""
-
-while word:
-    position = random.randrange(len(word))
-    pomie += word[position]
-    word = word[:position] + word[(position+1):]
+class Guesser:
+    def __init__(self):
+        self.score = 0
 
 
-print("""
-    Witaj w grze 'Wymieszane litery'!
-    Uporządkuj litery, aby odtworzyć prawidłowe słowo.
-    (Aby zakończyć zgadywanie, naciśnij klawisz Enter bez podawania odpowiedzi.)
-""")
+        self.words = ("python","gdansk","dlaczego","gdynia","wsb")
+        print("""
+            Witaj w grze 'Wymieszane litery'!
+            Uporządkuj litery, aby odtworzyć prawidłowe słowo.
+            (Aby zakończyć zgadywanie, naciśnij klawisz Enter bez podawania odpowiedzi.)
+        """)
+        self.correct_word = ""
+        self.mixed = ""
+        self.answer = ""
+        self.mixWords()
+        self.getAnswer()
+        self.checkAnswer()
 
-print("Zgadnij, jakie to słowo: %s" %(pomie))
+    def giveHint(self,char):
+        'Method to gave hints if player asks for it'
+        print("Jest tam znak: %s" % (char))
 
-zgaduj = input("\n Twoja odpowiedź: ")
-while zgaduj != poprawnie and zgaduj != "":
-    print("Niestety to nie to słowo.")
-    zgaduj = input("Twoja odpowiedź: ")
-if zgaduj == poprawnie:
-    print("Zgada się! Zgadłeś!\n")
-print("Dziękuję za udział w grze")
+    def mixWords(self):
+        'Method to get one word, and mix it'
+        word = random.choice(self.words)
+        self.correct_word = word
+
+        while word:
+            position = random.randrange(len(word))
+            self.mixed += word[position]
+            word = word[:position] + word[(position + 1):]
+
+    def getAnswer(self):
+        print("Zgadnij, jakie to słowo: %s" % (self.mixed))
+        self.answer = input("\n Twoja odpowiedź: ")
+
+    def checkAnswer(self):
+
+        while self.answer != self.correct_word and self.answer != "":
+            print("Niestety to nie to słowo.")
+            print("Czy chcesz skorzystać z podpowiedzi?")
+            choice = input("T/N")
+            if (choice.upper() == "t".upper()):
+                self.giveHint(self.mixed[random.randint(0, len(self.mixed) - 1)])
+            self.answer = input("Twoja odpowiedź: ")
+        if(self.answer == self.correct_word):
+            print("Zgada się! Zgadłeś!\n")
+
+game = Guesser()
