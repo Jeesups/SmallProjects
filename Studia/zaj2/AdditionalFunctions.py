54baff46a1a9360b85a64f537ea55f1abed6aefc
Patryk Weber <weberpatryk7@gmail.com>
2018-10-21 18:41:12 +0200

    Filling Statistic methods

--- Studia/zaj2/AdditionalFunctions.py
+++ Studia/zaj2/AdditionalFunctions.py
@@ -1,25 +1,45 @@
 import statistics #Jezeli chcemy zrobic to szybciej
 import math
 
-def median(list):
+def median(val):
     result = 0
     #Gdy parzysta ilosc elementow
-    if(len(list)%2 == 0):
-        result = list[math.floor(len(list/2))]+list[math.ceil(len(list)/2)]
+    if(len(val)%2 == 0):
+        result = val[math.floor(len(val)/2)-1]+val[math.ceil(len(val)/2)]/2
 
     #Gdy nieparzysta ilosc elementow
-    if (len(list)%2 != 0):
-        result = list[len(list / 2)]
+    if (len(val)%2 != 0):
+        result = val[int(len(val) / 2)]
 
     return result
 
-def avg(list):
+def avg(val):
     result = 0
     temp = 0
-    for i in list:
+    for i in val:
         temp += i
     result = temp / 2
     return result
 
-def stand_deviation():
-    pass
+def stand_deviation(val):
+    result = 0
+    temp = 0 
+    temp_list = val
+    me = mean(val)
+    
+    for i in range(len(val)):
+        temp_list[i] = (temp_list[i] - me) ** 2
+        temp += temp_list[i]
+
+    result = math.sqrt(temp/(len(val)-1))
+    return result
+
+#Support function
+def mean(val):
+    result = 0
+    temp = 0
+    for i in val:
+        temp += i
+    result = temp / len(val)
+    return result
+
--- Studia/zaj2/MarksMedian.py
+++ Studia/zaj2/MarksMedian.py
@@ -1,2 +1,18 @@
-import AdditionalFunctions
+import AdditionalFunctions as AF
+import statistics as st
+
+example_list_odd = [20,34,156,12,45]
+example_list_even = [56,32,14,67,43,24]
+
+#Mediany
+print(AF.median(example_list_even))
+print(AF.median(example_list_odd))
+
+#Odchylenie standardowe
+print(AF.stand_deviation(example_list_even))
+print(AF.stand_deviation(example_list_odd))
+
+#Srednia
+print(AF.avg(example_list_even))
+print(AF.avg(example_list_odd))
 
